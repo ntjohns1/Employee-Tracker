6b0e34fd7a331156b0c2bcd5cd2fcfd9bd1aba75
ntjohns1 <nelsontjohns@gmail.com>
2021-04-27 13:27:47 -0400

    working on getting employees by role

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const mysql = require('mysql2');
 const consoleTable = require('console.table');
 const connection = mysql.createConnection({
     host: 'localhost',
-  
+
     // Your port; if not 3306
     port: 3306,
-  
+
     // Your username
     user: 'root',
-  
+
     // Be sure to update with your own MySQL password!
     password: 'WorldWideWebster3',
     database: 'employeesDB',
-  });
-  
-  connection.connect((err) => {
+});
+
+connection.connect((err) => {
     if (err) throw err;
     start();
-  });
+});
 // const { type } = require('node:os');
 // const { rawListeners } = require('node:process');
 
@@ -50,9 +50,9 @@ const start = () => {
                 case 'view all employees':
                     viewEmployees();
                     break;
-                // case 'view all employees by role':
-                //     employeesByRole();
-                //     break;
+                case 'view all employees by role':
+                    employeesByRole();
+                    break;
                 // case 'view all employees by department':
                 //     employeesByDept();
                 //     break;
@@ -75,22 +75,38 @@ const start = () => {
 const viewEmployees = () => {
     const query = 'SELECT * FROM employee';
     connection.query(query, (err, res) => {
-        res.forEach(({ first_name, last_name, role_id, manager_id }) => {
-        const values = [`${first_name}`, `${last_name}`, `${role_id}`, `${manager_id}`]
-        console.table(['First Name', 'Last Name', 'Role', 'Manager'], [values])
+        console.table(res);
     });
-        start();
+    start();
+};
+
+const employeesByRole = () => {
+    connection.query('SELECT * FROM role', (err, res) => {
+        if (err) throw err;
+        inquirer.prompt(
+            {
+                name: 'select_role',
+                type: 'rawlist',
+                message: 'What role do you want to list employees by?',
+                choices() {
+                    const choiceArray = [];
+                    res.forEach(({ title }) => {
+                        choiceArray.push(title);
+                    });
+                    return choiceArray;
+                },
+            })
+            .then((answer) => {
+                // take answer from prompt compare title to id in role table
+                // return all employees with that employee.role_id
+                const query = 'SELECT id, first_name, last_name FROM role JOIN employee ON role WHERE role.id = role_id';
+                connection.query(query, (err, res) => {
+                    console.table(res);
+                });
+            });
     });
 };
 
-// const employeesByRole = () => {
-//     inquirer.prompt([
-
-//     ])
-//         .then((answers) => {
-
-//         });
-// };
 // const employeesByDept = () => {
 //     inquirer.prompt([
 
--- seed.sql
+++ seed.sql
@@ -1,10 +1,10 @@
 USE employeesDB;
 
 INSERT INTO department (name)
-VALUES ('Engineering'), ('Accounting');
+VALUES ('Admin'), ('Development'), ('Sales'), ('Warehouse'), ('HR'), ('Accounting');
 
 INSERT INTO role (title, salary, department_id)
-VALUES ('Project Manager', 80000, 2);
+VALUES ('District Manager', 120000, 1), ('Senior Engineer', 110000, 2), ('Sales Manager', 100000, 3), ('Warehouse Manager', 90000, 4), ('HR Manager', 90000, 5), ('Accounting Manager', 90000, 6), ('Receptionist', 50000, 1), ('Developer', 70000, 2), ('Sales Associate', 70000, 3), ('Warehouse Associate', 50000, 4), ('HR Rep', 70000, 5), ('Accountant', 70000, 6);
 
-INSERT INTO employee (first_name, last_name, role_id)
-VALUES ('Bob', 'Smith', 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Michael', 'Scott', 1, null), ('Jim', 'Jimson', 2, 1), ('Dwight', 'Schrute', 3, 1), ('Darryl', 'Philbin', 4, 1), ('Toby', 'Flenderson', 5, 1), ('Oscar', 'Nunez', 6, 1), ('Pam', 'Beesley', 7, 1), ('Jen', 'Jenson', 8, 2), ('Jim', 'Halpert', 9, 3), ('Rick', 'Dickson', 10, 4), ('Bob', 'Robertson', 11, 5), ('Angela', 'Martin', 12, 6);
